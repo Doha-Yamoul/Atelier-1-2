9ce91284a12770f77fb8c15aaa97be6d2cb1941e
DESKTOP-2LEBMCH\pc <YAMOUL_DOHA@emsi-edu.ma>
2022-11-01 00:14:15 +0100

    FC

---
+++ bank-account-service/src/main/java/org/sid/bankaccountservice/BankAccountServiceApplication.java
@@ -0,0 +1,19 @@
+package org.sid.bankaccountservice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BankAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountServiceApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start() {
+
+    }
+
+}
